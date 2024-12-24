846fe3588013be2e0ba0aa4280351bccbda9c124
Raghav Sharma <rs0077@srmist.edu.in>
2024-12-24 23:42:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const revealMessageBtn = document.getElementById("revealMessageBtn");
+const specialMessage = document.getElementById("specialMessage");
+
+revealMessageBtn.addEventListener("click", function () {
+  specialMessage.style.display = "block";
+});
